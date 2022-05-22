ccb2b78712da7b625683bc39065ba2e4ca8547fa
zackckpetersen <zackcpetersen@gmail.com>
2022-05-22 10:33:08 -0600

    adding model methods and serializers

--- quotes/serializers.py
+++ quotes/serializers.py
@@ -1,9 +1,39 @@
+from datetime import date
+
 from rest_framework import serializers
 
 from quotes.models import Quote
 
 
 class QuoteSerializer(serializers.ModelSerializer):
+    quote_id = serializers.ReadOnlyField()
+    base_fee = serializers.ReadOnlyField()
+    policy_term = serializers.ReadOnlyField()
+
     class Meta:
         model = Quote
         fields = '__all__'
+
+    def validate_effective_date(self, effective_date):
+        """
+        Validate that effective date coming in from serializer is in the future
+        """
+        today = date.today()
+        if effective_date < today:
+            raise serializers.ValidationError('Effective date cannot be in the past')
+        return effective_date
+
+
+class CheckoutQuoteSerializer(serializers.ModelSerializer):
+    base_premium = serializers.DecimalField(max_digits=5, decimal_places=2)
+    total_term_premium = serializers.DecimalField(max_digits=5, decimal_places=2)
+    total_monthly_premium = serializers.DecimalField(max_digits=5, decimal_places=2)
+    total_additional_term_fees = serializers.DecimalField(max_digits=5, decimal_places=2)
+    total_monthly_fees = serializers.DecimalField(max_digits=5, decimal_places=2)
+    total_term_discounts = serializers.DecimalField(max_digits=5, decimal_places=2)
+    total_monthly_discounts = serializers.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        model = Quote
+        fields = ['base_premium', 'total_term_premium', 'total_monthly_premium', 'total_additional_term_fees',
+                  'total_monthly_fees', 'total_term_discounts', 'total_monthly_discounts']
--- quotes/views.py
+++ quotes/views.py
@@ -1,12 +1,28 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets, views
+from rest_framework.response import Response
 
+from quotes.forms import CheckoutQuoteForm
 from quotes.models import Quote
-from quotes.serializers import QuoteSerializer
+from quotes.serializers import CheckoutQuoteSerializer, QuoteSerializer
 
 
-class QuoteViewSet(viewsets.ModelViewSet):
+class QuoteViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
     """
-    API endpoint that allows full CRUD for Quotes
+    API endpoint that allows `create`, `retrieve`, `delete`
+    We do not want to allow `update` according to project reqs
     """
     queryset = Quote.objects.all()
     serializer_class = QuoteSerializer
+
+
+class CheckoutQuoteView(views.APIView):
+    def get(self, request):
+        form = CheckoutQuoteForm(request.GET)
+        if form.is_valid():
+            serializer = CheckoutQuoteSerializer(form.cleaned_data.get('quote'))
+            return Response(status=200, data=serializer.data)
+        return Response(status=400, data=form.errors)
