10274b5bb21679b2f304628a39dc373b8882a728
zackckpetersen <zackcpetersen@gmail.com>
2022-05-22 10:35:14 -0600

    adding form to validate quote id for checkout quote endpoint

---
+++ quotes/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+from quotes.models import Quote
+
+class CheckoutQuoteForm(forms.Form):
+    quote = forms.ModelChoiceField(queryset=Quote.objects.all(), to_field_name='quote_id')
